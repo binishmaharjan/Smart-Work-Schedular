23ee0b75420e4fdcb519dc0d6b439822dbf8df5d
Binish Maharjan <binish.maharjan.lj@ambl.co.jp>
2024-07-21 16:30:25 +0900

    add sharedUIs module

--- App/SmartWorkSchedular/Package.swift
+++ App/SmartWorkSchedular/Package.swift
@@ -8,14 +8,20 @@ let package = Package(
     defaultLocalization: "en",
     platforms: [.iOS(.v17), .macOS(.v12)],
     products: [
-        .library(
-            name: "SmartWorkSchedular",
-            targets: ["SmartWorkSchedular"]
-        ),
+        .library(name: "SmartWorkSchedular", targets: ["SmartWorkSchedular"]),
+        .library(name: "SharedUIs", targets: ["SharedUIs"]),
     ],
     targets: [
         .target(
             name: "SmartWorkSchedular"
+        ),
+        .target(
+            name: "SharedUIs",
+            dependencies: [
+            ],
+            resources: [
+                .process("Resources"),
+            ]
         ),
         .testTarget(
             name: "SmartWorkSchedularTests",
