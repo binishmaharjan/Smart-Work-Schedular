a52706e488614ccb738d1cfb1d833785dcfdda7e
Binish Maharjan <binish.maharjan.lj@ambl.co.jp>
2024-07-27 14:37:38 +0900

    update swift syntax version

--- App/SmartWorkSchedular/Package.swift
+++ App/SmartWorkSchedular/Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         .library(name: "AppFeature", targets: ["AppFeature"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-syntax.git", from: "509.0.0"),
+        .package(url: "https://github.com/apple/swift-syntax.git", from: "510.0.2"),
     ],
     targets: [
         .target(
