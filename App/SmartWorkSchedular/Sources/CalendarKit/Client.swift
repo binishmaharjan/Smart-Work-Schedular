8974606ab17c13fdc9914580f0730b92ca6565b3
Binish Maharjan <binish.maharjan.lj@ambl.co.jp>
2024-09-08 15:29:03 +0900

    add dependencyClient

--- App/SmartWorkSchedular/Sources/CalendarKit/Client.swift
+++ App/SmartWorkSchedular/Sources/CalendarKit/Client.swift
@@ -1,11 +1,14 @@
 import Foundation
 import Dependencies
+import DependenciesMacros
 
+@DependencyClient
 public struct CalendarKitClient {
     /// Returns the display days for the calendar according to the current mode
-    public var displayDays: (_ from: Day) -> [Day]
-    public var nextFocusDay: (_ from: Day) -> Day
-    public var previousFocusDay: (_ from: Day) -> Day
+    public var displayDays: (_ from: Day) -> [Day] = { _ in [] }
+    public var nextFocusDay: (_ from: Day) -> Day = { _ in .init(date: .now) }
+    public var previousFocusDay: (_ from: Day) -> Day = { _ in .init(date: .now) }
+    public var updateStartWeekdayOn: (_ to: Weekday) -> Void
 }
 
 // MARK: Dependency Values
@@ -18,15 +21,13 @@ extension DependencyValues {
 
 // MARK: Dependency (testValue, previewValue)
 extension CalendarKitClient: TestDependencyKey {
-    public static var testValue = CalendarKitClient(
-        displayDays: unimplemented("\(Self.self).displayDays is unimplemented", placeholder: []),
-        nextFocusDay: unimplemented("\(Self.self).nextFocusDay is unimplemented", placeholder: Day(date: .now)),
-        previousFocusDay: unimplemented("\(Self.self).previousFocusDay is unimplemented", placeholder: Day(date: .now))
-    )
+    public static var testValue = Self()
+    public static var previewValue = Self.noop
     
-    public static var previewValue = CalendarKitClient(
-        displayDays: unimplemented("\(Self.self).displayDays is unimplemented", placeholder: []),
-        nextFocusDay: unimplemented("\(Self.self).nextFocusDay is unimplemented", placeholder: Day(date: .now)),
-        previousFocusDay: unimplemented("\(Self.self).previousFocusDay is unimplemented", placeholder: Day(date: .now))
+    public static var noop = CalendarKitClient(
+        displayDays:  { _ in [] },
+        nextFocusDay: { _ in .init(date: .now) },
+        previousFocusDay: { _ in .init(date: .now) },
+        updateStartWeekdayOn: { _ in }
     )
 }
