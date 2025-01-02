73fa3ee467632524a040cf7f44549cc46497afb1
Binish Maharjan <binish.maharjan.lj@ambl.co.jp>
2025-01-02 16:56:36 +0900

    create a public init

--- App/SmartWorkSchedular/Sources/SharedModels/Entry.swift
+++ App/SmartWorkSchedular/Sources/SharedModels/Entry.swift
@@ -6,16 +6,44 @@ public enum EntryType {
 }
 
 public struct Entry: Identifiable {
+    public init(
+        id: UUID,
+        title: String,
+        icon: String,
+        isAllDay: Bool = false,
+        startDate: String,
+        endDate: String,
+        breakTime: Double? = nil,
+        notes: String? = nil,
+        location: String? = nil,
+        entryType: EntryType,
+        wagePerHour: Double? = nil,
+        creationDate: Date
+    ) {
+        self.id = id
+        self.title = title
+        self.icon = icon
+        self.isAllDay = isAllDay
+        self.startDate = startDate
+        self.endDate = endDate
+        self.breakTime = breakTime
+        self.notes = notes
+        self.location = location
+        self.entryType = entryType
+        self.wagePerHour = wagePerHour
+        self.creationDate = creationDate
+    }
+
     public var id: UUID
     public var title: String
     public var icon: String
     public var isAllDay: Bool
-    public var startDate: Date
-    public var endDate: Date
-    public var breakTime: Int
-    public var notes: String
-    public var location: String
+    public var startDate: String
+    public var endDate: String
+    public var breakTime: Double?
+    public var notes: String?
+    public var location: String?
     public var entryType: EntryType
-    public var wagePerHour: Double
+    public var wagePerHour: Double?
     public var creationDate: Date
 }
