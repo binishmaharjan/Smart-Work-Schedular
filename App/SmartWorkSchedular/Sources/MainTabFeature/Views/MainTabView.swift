f21848b3daa99b3e04c9e0a55dbd3bea12c200c8
Binish Maharjan <binish.maharjan.lj@ambl.co.jp>
2025-01-03 09:11:58 +0900

    fix colors

--- App/SmartWorkSchedular/Sources/MainTabFeature/Views/MainTabView.swift
+++ App/SmartWorkSchedular/Sources/MainTabFeature/Views/MainTabView.swift
@@ -49,7 +49,7 @@ public struct MainTabView: View {
             reducer: MainTab.init
         )
     )
-    .tint(.accentColor)
+    .tint(Color.accent)
 }
 
 #Preview("Dark Mode") {
@@ -59,6 +59,6 @@ public struct MainTabView: View {
             reducer: MainTab.init
         )
     )
-    .tint(.accentColor)
+    .tint(Color.accent)
     .preferredColorScheme(.dark)
 }
