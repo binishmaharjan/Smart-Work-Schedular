876d6aba3f46b8c92abc3fa970859a256fe84621
Binish Maharjan <binish.maharjan.lj@ambl.co.jp>
2025-01-03 09:30:29 +0900

    add navigation view

--- App/SmartWorkSchedular/Sources/TemplatesFeature/Core/Templates.swift
+++ App/SmartWorkSchedular/Sources/TemplatesFeature/Core/Templates.swift
@@ -1,21 +1,35 @@
 import ComposableArchitecture
 import Foundation
+import NavigationBarFeature
 
 @Reducer
 public struct Templates {
     @ObservableState
     public struct State: Equatable {
         public init() { }
+        
+        var navigationBar = NavigationBar.State(
+            title: "Templates",
+            firstTrailingItem: "plus"
+        )
     }
     
     public enum Action {
+        case navigationBar(NavigationBar.Action)
     }
     
     public init() { }
     
     public var body: some ReducerOf<Self> {
-        Reduce<State, Action> { _, _ in
-            .none
+        Reduce<State, Action> { _, action in
+            switch action {
+            case .navigationBar:
+                return .none
+            }
+        }
+        
+        Scope(state: \.navigationBar, action: \.navigationBar) {
+            NavigationBar()
         }
     }
 }
--- App/SmartWorkSchedular/Sources/TemplatesFeature/Views/TemplatesView.swift
+++ App/SmartWorkSchedular/Sources/TemplatesFeature/Views/TemplatesView.swift
@@ -1,4 +1,5 @@
 import ComposableArchitecture
+import NavigationBarFeature
 import SharedUIs
 import SwiftUI
 
@@ -7,14 +8,33 @@ public struct TemplatesView: View {
         self.store = store
     }
     
-    @Bindable private var store: StoreOf<Templates>
+    @Bindable public var store: StoreOf<Templates>
     
     public var body: some View {
-        Text("Templates View")
-            .font(.customTitle)
-            .foregroundStyle(Color.text)
+        NavigationStack {
+            VStack {
+                Text("Templates View")
+                    .font(.customTitle)
+                    .foregroundStyle(Color.text)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .background(Color.background)
+            }
             .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .padding(.top, 50) // Takes space for navigation bar
+            .padding(.top, 8)
             .background(Color.background)
+            .overlay(navigationBar)
+        }
+    }
+}
+
+// MARK: Views
+extension TemplatesView {
+    @ViewBuilder
+    private var navigationBar: some View {
+        NavigationBarView(
+            store: store.scope(state: \.navigationBar, action: \.navigationBar)
+        )
     }
 }
 
