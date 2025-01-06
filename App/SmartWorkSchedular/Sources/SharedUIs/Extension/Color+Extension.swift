1505d12629fdc6ef87d27e3d07b85e63f309d190
Binish Maharjan <binish.maharjan.lj@ambl.co.jp>
2025-01-06 16:40:01 +0900

    add extension to create color from hexCode

--- App/SmartWorkSchedular/Sources/SharedUIs/Extension/Color+Extension.swift
+++ App/SmartWorkSchedular/Sources/SharedUIs/Extension/Color+Extension.swift
@@ -1,5 +1,6 @@
 import SwiftUI
 
+// MARK: Predefined Colors
 extension Color {
     /// Light: #F4C33E, Dark: #F4C33E
     public static let background = #color("background")
@@ -9,8 +10,22 @@ extension Color {
     public static let accent = #color("accent_color")
     /// Light: #B5B5B5, Dark: #F5F5F5
     public static let separator = #color("separator")
-    ///Light: #8E8E93, Dark: #8E8E93
+    /// Light: #8E8E93, Dark: #8E8E93
     public static let subText = #color("sub_text_color")
     /// Light: #1E1E2D, Dark: #FEFEFF
     public static let text = #color("text_color")
+}
+
+// MARK: Hex Code
+extension Color {
+    /// Initializes a new UIColor instance from a hex string
+    public init(hex: String) {
+        let uiColor = UIColor(hex: hex)
+        self.init(uiColor: uiColor)
+    }
+
+    /// Returns a hex string representation of the UIColor instance
+    public func toHexString(includeAlpha: Bool = false) -> HexCode? {
+        UIColor(self).toHexString(includeAlpha: includeAlpha)
+    }
 }
